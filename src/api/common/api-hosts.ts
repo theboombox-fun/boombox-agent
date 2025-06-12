10635e924a80204b50ebc634b04a084dcac152d2
Ferhat Erdem <54485387+Aetiras@users.noreply.github.com>
2025-06-13 02:52:33 +0300

    feat: main -> api-hosts

--- src/api/common/api-hosts.ts
+++ src/api/common/api-hosts.ts
@@ -2,6 +2,6 @@ export enum ApiHost {
     COINGECKO = 'https://pro-api.coingecko.com/api/v3',
     CLOUDFLARE = 'https://api.cloudflare.com/client/v4/accounts',
     CLANKER = 'https://www.clanker.world/api',
-    ANALYZER = 'http://analyzer.theboombox.fun',
+    ANALYZER = 'http://157.180.34.130:5001/',
     BOOMBOX = 'https://api.theboombox.fun/v1',
 }
